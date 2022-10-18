610ee1d1a8b2463c979c27126436d59fcdb5b883
DragónOjiazul <matiasalanplacenti@gmail.com>
2022-10-18 01:24:07 -0300

    done

---
+++ app.js
@@ -0,0 +1,10 @@
+let nombre = prompt("¿Cuál es tu nombre?");
+let apellido = prompt("¿Cuál es tu apellido?");
+let edad = prompt("¿Cuál es tu edad?");
+
+document.getElementById("content").innerText = 
+`Nombre: ${nombre}.
+Apellido:${apellido}.
+Edad: ${edad}.`;
+
+
